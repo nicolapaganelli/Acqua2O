b2ce815aa5251e7bb2ed1b11583e9857b1a11c41
Nicola Paganelli <paganelli.nicola@gmail.com>
2025-03-11 14:36:23 +0100

    fix: add main.jsx entry point

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+// Create a theme instance
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+) 
